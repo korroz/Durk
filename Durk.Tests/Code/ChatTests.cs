b8c82beefb2babe6db38ba46c89075acf9d6871c
Mats Näsström <korroz.with.git@voidentity.net>
2011-10-05 07:49:51 +0200

    Creates test helpers
    Some helper classes to make it easier to unit test the server side
    signalR stuff.

--- Durk.Tests/Code/ChatTests.cs
+++ Durk.Tests/Code/ChatTests.cs
@@ -13,14 +13,25 @@ namespace Durk.Tests.Code
 {
 	public class ChatTests
 	{
-		#region Utility
+		#region Test helpers
 
-		protected static class Util
+		public static class Util
 		{
 		}
 
-		public class TestAgent : IClientAgent
+		public class TestAgent : DynamicObject, IClientAgent
 		{
+			public class CallData
+			{
+				public List<object[]> CallArguments = new List<object[]>();
+
+				public T GetArgument<T>(int argIndex = 0, int callIndex = 0)
+				{
+					return (T)CallArguments[callIndex][argIndex];
+				}
+			}
+
+			private Dictionary<string, CallData> _calls = new Dictionary<string, CallData>();
 			#region IClientAgent Members
 
 			public System.Threading.Tasks.Task Invoke(string method, params object[] args)
@@ -30,14 +41,49 @@ namespace Durk.Tests.Code
 
 			#endregion
 
-			public Action<string> addMessageAction;
-			public void addMessage(string message)
+			public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
 			{
-				if (addMessageAction != null)
-					addMessageAction(message);
+				var calldata = _calls.ContainsKey(binder.Name) ? _calls[binder.Name] : new CallData();
+				calldata.CallArguments.Add(args);
+				_calls[binder.Name] = calldata;
+
+				result = null;
+				return true;
+			}
+
+			public bool WasCalled(string methodName)
+			{
+				return _calls.ContainsKey(methodName);
+			}
+
+			public CallData CalledMethod(string methodName)
+			{
+				return _calls[methodName];
 			}
 		}
 
+		#endregion
+
+		#region Test for the test helpers
+		[Fact]
+		public void TestAgent_Should_Save_Info_On_Dynamic_Calls()
+		{
+			var agent = new TestAgent();
+
+			((dynamic)agent).someMethod();
+
+			Assert.True(agent.WasCalled("someMethod"));
+		}
+
+		[Fact]
+		public void TestAgent_Should_Provide_Dynamic_Call_Arguments()
+		{
+			var agent = new TestAgent();
+
+			((dynamic)agent).someMethod(arg: "Some String");
+
+			Assert.True(agent.CalledMethod("someMethod").GetArgument<string>() == "Some String");
+		}
 		#endregion
 
 		[Fact]
@@ -45,13 +91,13 @@ namespace Durk.Tests.Code
 		{
 			var chat = new Chat();
 			var message = "Some line";
-			var messageOut = "";
-			chat.Agent = new TestAgent { addMessageAction = s => messageOut = s };
+			var agent = new TestAgent();
+			chat.Agent = agent;
 			chat.Context = new HubContext(null, null, new GenericPrincipal(new GenericIdentity("SomeUser"), null));
 
 			chat.Send(message);
 
-			Assert.False(messageOut == "");
+			Assert.False(agent.CalledMethod("addMessage").GetArgument<string>() == "");
 		}
 	}
 }
